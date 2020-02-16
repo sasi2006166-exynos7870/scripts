e45bf35bda4b764918b1db78bf35d93c81b31cdc
Salvatore Fornaro <49954132+sasi2006166@users.noreply.github.com>
2020-02-16 22:21:02 +0100

    Exynos7870: remove devices
    I'm deleting devices because i will make a totally new file that selects what to copy :)

--- 2.Copying.sh
+++ 2.Copying.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo  "Made By @sasi2006166"
-echo -e "github.com/sasi2006166\n"  
-echo "Before Using this script you need packages.sh" 
+echo -e "github.com/sasi2006166\n"
+echo "Before Using this script you need packages.sh"
 
-echo "Select what LineageOS version do you want to build" 
-echo " 1) lineage-15.1       2) lineage-16.0       3) lineage-17.0" 
+echo "Select what LineageOS version do you want to build"
+echo " 1) lineage-15.1       2) lineage-16.0       3) lineage-17.0"
 
 
 
@@ -12,7 +12,7 @@ while   :
 do
     read INPUT_STRING
     case $INPUT_STRING in
-         1) 
+         1)
             echo -e "\nOreo Selected\n\nmaking dir...\n"
             mkdir lineage-15_1
             cd $(pwd)/lineage-15_1/
@@ -25,89 +25,37 @@ do
              ;;
 
          2)
-	    
+
             echo -e "\nPie Selected\n\nmaking dir...\n"
-            mkdir lineage-16_0 
+            mkdir lineage-16_0
             cd $(pwd)/lineage-16_0
 	    bs_dir1=$(pwd)
 	    echo -e "\n\nSyncing....\n\n======\n"
-            repo init -u git://github.com/LineageOS/android.git -b lineage-16.0 
+            repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
             repo sync -j$(($(nproc)+1))
 	    echo -e "\n======\n"
 	    break
              ;;
-             
+
          3)
-            echo "\nQ Selected\n\nmaking dir...\n" 
-            mkdir lineage-17_0  
+            echo "\nQ Selected\n\nmaking dir...\n"
+            mkdir lineage-17_0
             cd $(pwd)/lineage-17_0
 	    echo "current dir $(pwd)"
 	    bs_dir2=$(pwd)
 	    echo -e "\n\nSyncing....\n\n======\n"
-            repo init -u git://github.com/LineageOS/android.git -b lineage-17.0 
+            repo init -u git://github.com/LineageOS/android.git -b lineage-17.0
             repo sync -j$(($(nproc)+1))
 	    echo -e "\n======\n"
 	    break
              ;;
-     
+
          *)
            echo "Sorry I can't understand, type again :)"
             ;;
-  esac 
+  esac
  done
-           
-
-echo -e "Sync Completed\n\n Now Select One of Exynos7870 devices listed Below :)" 
-echo "1) j7xelte       2) j7velte       3) on7xelte" 
 
 
-
-while  : 
-do 
-   read INPUT_STRING
-   case $INPUT_STRING in 
-           1)
-               echo -e "\nJ7 2016 Selected\n\nmaking dir...\n"
-               mkdir -p .repo/local_manifests/
-               cd $(pwd)/.repo/local_manifests
-	       echo -e "\ngetting manifests\n\n======\n"
-               wget https://github.com/sasi2006166-exynos7870/local_manifests/j7xelte.xml
-               cd $bs_dir 
-               echo -e "\nSyncing...\n\n======\n"
-               repo sync -j$(($(nproc)+1))
-	       echo -e "\n======\n"
-	       break
-               ;;
-           
-           2)
-               echo -e "\nJ7 Prime Selected\n\nmaking dir...\n" ;
-               mkdir -p .repo/local_manifests/
-               cd $(pwd)/.repo/local_manifests
-	       echo -e "\ngetting manifests\n\n======\n"
-               wget https://github.com/sasi2006166-exynos7870/local_manifests/on7xelte.xml
-               cd $bs_dir1
-	       echo -e "Syncing....\n\n======\n"
-               repo sync -j$(($(nproc)+1))
-	       echo -e "\n======\n"
-	       break
-               ;;
-
-            3)
-               echo "\nJ7 NXT Selected\n\nmaking dir...\n" ;
-               mkdir -p .repo/local_manifests/
-               cd $(pwd)/.repo/local_manifests
-	       echo -e "\ngetting manifests\n\n======"
-               wget https://github.com/sasi2006166-exynos7870/local_manifests/j7velte.xml
-               cd $bs_dir2
-	       echo -e "Syncing....\n\n======\n"
-               repo sync -j$(($(nproc)+1))
-	       break
-               ;;
-
-
-
-   esac 
- done
 echo -e "\n\nEverything is fine!"
 echo "Now move to build.sh file"
-
